9a17a336b031eb5d6ced019e476524655a099c1c
View Only User <view-only@livefire.solutions>
2021-04-08 07:18:37 -0700

    Update main.tf

--- terraform/skaloferov/main.tf
+++ terraform/skaloferov/main.tf
@@ -19,11 +19,11 @@ resource "aws_security_group" "class_delivery_sg" {
 resource "aws_security_group_rule" "ingress_rules" {                                  
   type              = "ingress"
 
-  from_port         = "666"
-  to_port           = "666"
-  protocol          = "TCP"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "The Port Of The Beast"
+  from_port         = var.awsSgIngressRules[0].from_port
+  to_port           = var.awsSgIngressRules[0].to_port
+  protocol          = var.awsSgIngressRules[0].protocol
+  cidr_blocks       = ["${var.awsSgIngressRules[0].cidr_blocks}"]
+  description       = var.awsSgIngressRules[0].description
 
   security_group_id = aws_security_group.class_delivery_sg.id                           # Security Group ID to which to attach 
   depends_on        = [aws_security_group.class_delivery_sg]                            # SG needs to exist first
@@ -33,11 +33,11 @@ resource "aws_security_group_rule" "ingress_rules" {
 resource "aws_security_group_rule" "egress_rules" {                                   
   type              = "egress"
 
-  from_port         = "666"
-  to_port           = "666"
-  protocol          = "TCP"
-  cidr_blocks       = ["0.0.0.0/0"]
-  description       = "The Port Of The Beast"
+  from_port         = var.awsSgIngressRules[0].from_port
+  to_port           = var.awsSgIngressRules[0].to_port
+  protocol          = var.awsSgIngressRules[0].protocol
+  cidr_blocks       = ["${var.awsSgIngressRules[0].cidr_blocks}"]
+  description       = var.awsSgIngressRules[0].description
   
   security_group_id = aws_security_group.class_delivery_sg.id                           # Security Group ID to which to attach 
   depends_on        = [aws_security_group.class_delivery_sg]                            # SG needs to exist first
