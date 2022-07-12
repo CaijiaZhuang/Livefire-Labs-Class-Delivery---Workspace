bf54c02a723f800c1b35b7efd8073dac095d33c5
View Only User <view-only@livefire.solutions>
2022-07-12 04:52:47 -0700

    Update main.tf

--- terraform/miuser006/main.tf
+++ terraform/miuser006/main.tf
@@ -37,3 +37,7 @@ resource "aws_security_group_rule" "egress_rules" {
   security_group_id = aws_security_group.class_delivery_sg.id                           # Security Group ID to which to attach 
   depends_on        = [aws_security_group.class_delivery_sg]                            # SG needs to exist first
 }
+# Get Data for instance(s) id(s)
+data "aws_instance" "instance" {
+  instance_id = var.awsInstanceId1
+}
