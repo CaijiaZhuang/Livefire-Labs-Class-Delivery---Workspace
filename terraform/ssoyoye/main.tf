72f7047036b9c9519f1a6ee3b008245ab83feae8
View Only User <view-only@livefire.solutions>
2022-10-30 19:54:49 -0700

    Update main.tf

--- terraform/ssoyoye/main.tf
+++ terraform/ssoyoye/main.tf
@@ -19,11 +19,42 @@ locals {
   # SG Rules
   awsSgIngressRules_obj = jsondecode(var.awsSgIngressRules)   # Ingress
   awsSgEgressRules_obj  = jsondecode(var.awsSgEgressRules)    # Egress
+
+# Get Instance(s) Id(s)
+data "aws_instances" "instances" {
+  filter {
+    name   = "instance-id"
+    values = [
+      # Get all instance Id(s) from machine resource 
+      local.awsInstanceId1_hasMultiple == false ? local.awsInstanceId1_obj[0] : "",
+      local.awsInstanceId1_length == 2 ? local.awsInstanceId1_obj[0] : "", 
+      local.awsInstanceId1_length == 2 ? local.awsInstanceId1_obj[1] : "", 
+      local.awsInstanceId1_length == 3 ? local.awsInstanceId1_obj[0] : "", 
+      local.awsInstanceId1_length == 3 ? local.awsInstanceId1_obj[1] : "", 
+      local.awsInstanceId1_length == 3 ? local.awsInstanceId1_obj[2] : "", 
+      # Get all instance Id(s) from machine resource
+      local.awsInstanceId2_hasMultiple == false ? local.awsInstanceId2_obj[0] : "",
+      local.awsInstanceId2_length == 2 ? local.awsInstanceId2_obj[0] : "", 
+      local.awsInstanceId2_length == 2 ? local.awsInstanceId2_obj[1] : "", 
+      local.awsInstanceId2_length == 3 ? local.awsInstanceId2_obj[0] : "", 
+      local.awsInstanceId2_length == 3 ? local.awsInstanceId2_obj[1] : "", 
+      local.awsInstanceId2_length == 3 ? local.awsInstanceId2_obj[2] : "" ,
+      # Get all instance Id(s) from machine resource
+      local.awsInstanceId3_hasMultiple == false ? local.awsInstanceId3_obj[0] : "",
+      local.awsInstanceId3_length == 2 ? local.awsInstanceId3_obj[0] : "", 
+      local.awsInstanceId3_length == 2 ? local.awsInstanceId3_obj[1] : "", 
+      local.awsInstanceId3_length == 3 ? local.awsInstanceId3_obj[0] : "", 
+      local.awsInstanceId3_length == 3 ? local.awsInstanceId3_obj[1] : "", 
+      local.awsInstanceId3_length == 3 ? local.awsInstanceId3_obj[2] : "" 
+      ]
+  }
+}
   
-  
-  # Get Data for instance(s) id(s)
-data "aws_instance" "instance" {
-  instance_id = var.awsInstanceId1
+ # Get Data for instance(s) id(s)
+data "aws_instance" "instance" {         
+  # instance_id = var.awsInstanceId1                       
+  count       = length(data.aws_instances.instances.ids)
+  instance_id = data.aws_instances.instances.ids[count.index]
 }
 
 
@@ -71,8 +102,9 @@ resource "aws_security_group_rule" "egress_rules" {
 
 
 # Attach Instance(s) eni(s) to the Security Group
-resource "aws_network_interface_sg_attachment" "sg_attachment" {                        
+resource "aws_network_interface_sg_attachment" "sg_attachment" {             
+  count                = length(data.aws_instance.instance)                             # Iterate over all Instances           
   security_group_id    = aws_security_group.class_delivery_sg.id                        # SG ID
-  network_interface_id = data.aws_instance.instance.network_interface_id                # ENI ID
+  network_interface_id = data.aws_instance.instance[count.index].network_interface_id                # ENI ID
   depends_on           = [aws_security_group.class_delivery_sg]                         # SG needs to exist first 
 }
